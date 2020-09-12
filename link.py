036f41883908878e923fba38ecf1c0390ae606d0
Atron13 <67007631+Atron13@users.noreply.github.com>
2020-09-12 21:23:27 +0530

    Create link.py

---
+++ link.py
@@ -0,0 +1,11 @@
+links required to know
+https://www.geeksforgeeks.org/taking-multiple-inputs-from-user-in-python/?ref=lbp
+
+
+
+
+https://www.tutorialspoint.com/python3/python_lists.htm
+https://www.tutorialspoint.com/python3/python_basic_operators.htm
+https://www.tutorialspoint.com/python3/list_sort.htm
+https://www.geeksforgeeks.org/taking-multiple-inputs-from-user-in-python/?ref=lbp
+https://www.geeksforgeeks.org/what-does-the-if-__name__-__main__-do
